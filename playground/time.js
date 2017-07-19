df3aa4b7bb1cac7664b93fad213c8c701e7a4d49
MacBook Pro <apple@Macs-MacBook.local>
2017-07-19 13:50:52 +0545

    format timestamps using momentJS

---
+++ playground/time.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb']
+// console.log(date.getMonth());
+
+// var date=moment();
+// date.add(1, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY '));
+var createdAt = 
+
+console.log(moment().format('H:mm A'));
